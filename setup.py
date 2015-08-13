bce302ce8c2bb501b1f966b396004434059bf5ce
David Ko <david.dk.ko@gmail.com>
2015-08-13 16:00:26 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import re
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('linkbot_jig_programmer/linkbot_jig_programmer.py').read(),
+    re.M
+    ).group(1)
+
+setup(
+    name = "linkbot_jig_programmer",
+    packages = ["linkbot_jig_programmer", ],
+    version = version,
+    entry_points = {
+        "console_scripts": ['linkbot-jig-programmer=linkbot_jig_programmer.linkbot_jig_programmer:main']
+    },
+    install_requires = ["PyLinkbot >= 2.3.4", "pystk500v2 >= 0.2.1", "pyserial"],
+    description = "Tool for flashing Linkbot main-boards with a bootloader on"
+        "the main programming jig.",
+    zip_safe = False,
+    include_package_data = True,
+    author = "David Ko",
+    author_email = "david@barobo.com",
+    )
+
